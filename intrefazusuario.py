be0edab3af7d56f96292cd34f7680c05591ffb31
rech99 <53828060+rech99@users.noreply.github.com>
2020-10-01 15:06:37 -0700

    Add files via upload

---
+++ intrefazusuario.py
@@ -0,0 +1,20 @@
+from tkinter import *
+
+ventana = Tk()
+ventana.title("Mi ventana")
+ventana.geometry("800x600")
+
+etiqueta = Label(ventana, text="Esto es una etiqueta")
+etiqueta.pack()
+
+segundaEtiqueta = Label(ventana, text="Segunda etiqueta")
+segundaEtiqueta.pack()
+
+ingresoTexto = Entry(ventana)
+ingresoTexto.pack()
+
+boton = Button(ventana, text="Mi Boton")
+boton.pack()
+
+
+ventana.mainloop()
